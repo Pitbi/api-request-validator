9bd42e7cec458360b288bc6bd42f826c5f57c43b
Pierre Biezemans <pitbi@iMac-de-Pierre.local>
2018-03-12 15:08:20 +0100

    Bugfix: get key

--- src/Validator.js
+++ src/Validator.js
@@ -33,7 +33,7 @@ class Validator {
         const { validations, data } = this
         for (const key in validations) {
             if (this.isValid)
-                await this.validate({ ...validations[key], key }, _.get(key))
+                await this.validate({ ...validations[key], key }, _.get(data, key))
         }
     }
 
